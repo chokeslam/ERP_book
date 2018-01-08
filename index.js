cd3a6c3ee5f0ff8e8ac32e6daa1d50b7486db122
chokeslam <chokeslam5608@gmail.com>
2018-01-08 17:19:42 +0800

    index.html ->new input * 5 .  index.js -> input enter to next table

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 	$(document).ready(function() {
     
+    // focus學生編號條碼
+    $("#code").focus();
     $("#search").on('click',A1);
 	
+	//學生編號條碼輸入按ENTER 執行A1
 	$("#code").keypress(function(){
   		if (event.which === 13){
 			A1();
     	}   
   	});
+  	//書籍條碼ENTER後換行
+  	$("#test1").keypress(function(){
+  		if (event.which === 13){
+			$("#test2").focus();
+			
+			$("#test2").keypress(function(){
+  				if (event.which === 13){
+					$("#test3").focus();
+					
+					$("#test3").keypress(function(){
+  						if (event.which === 13){
+							$("#test4").focus();
+							
+							$("#test4").keypress(function(){
+  								if (event.which === 13){
+									$("#test5").focus();
+    							}   
+  							});
+    					}   
+  					});
+    			}   
+  			});
+    	}   
+  	});
+  	
+  	
+  	
+  	
+  	//將sever.php 處理好的資料 回傳
 	function A1 () {
 	  $.ajax({
             type: "GET",
@@ -15,7 +47,9 @@
             success: function(data) {
                 
                 if (data.code) {
-                	
+                	//focus 到書籍條碼的input
+                	$("#test1").focus();
+                	//列出資料
                 	$("img").attr("src",data.img);
                     
                     $("#searchname").html(
@@ -40,6 +74,7 @@
 					});
                  
                 } else {
+                	$("#code").focus();
                     $("#searchname").html(data.msg);
                 }
             },
